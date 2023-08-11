2be59cd01ae290ec3f0297e8057cc4a1489a6ff2
Md. Mujahidul Islam <112639470+SEBPO-mujahidul-islam@users.noreply.github.com>
2023-08-11 17:47:47 +0600

    merged all sections

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function($) {
+
+	"use strict";
+
+	$('nav .dropdown').hover(function(){
+		var $this = $(this);
+		$this.addClass('show');
+		$this.find('> a').attr('aria-expanded', true);
+		$this.find('.dropdown-menu').addClass('show');
+	}, function(){
+		var $this = $(this);
+			$this.removeClass('show');
+			$this.find('> a').attr('aria-expanded', false);
+			$this.find('.dropdown-menu').removeClass('show');
+	});
+
+})(jQuery);
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
